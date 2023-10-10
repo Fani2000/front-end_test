966fd4e4d0b4c67ce17aaa98d71b7b626ad55019
fkeorapetse <fkeorapetse@quickloc8.co.za>
2023-10-10 10:48:52 +0200

    Updted the header and also setup for the project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-primary px-16 items-center flex justify-between h-[80px]">
+      <a href="#">
+        <img className="" src="src/assets/brand/logo.svg" />
+      </a>
+      <div className="flex gap-6 font-bold text-white">
+        <span>Services</span>
+        <span>Industries</span>
+        <span>Cases</span>
+        <span>Contact</span>
+      </div>
+      <div className="rounded-full  p-2 px-6 bg-white text-primary font-bold">
+        Let's Talk
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HomeLayout from './layout/HomeLayout'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HomeLayout />
+  </React.StrictMode>
+)
